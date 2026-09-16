5ecb68649834d6b8e88eb4103aaed1bebbbacf0c
dave1 <dave112.ore@gmail.com>
2026-09-16 00:12:00 -0600

    add python code

---
+++ calc.py
@@ -0,0 +1,4 @@
+firstinputnumber=int(input("Enter your Numbeer   "))
+secondinputnumber=int(input("Enter your Numbeer  "))
+result=firstinputnumber+secondinputnumber
+print("your result is ",  result)
